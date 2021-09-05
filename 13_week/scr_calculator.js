cb7a7f468d380ec16c8a1e4ac12309b30aeddf4b
Anna <89824460102@yandex.ru>
2021-09-05 17:45:53 +0500

    JS

---
+++ 13_week/scr_calculator.js
@@ -0,0 +1,20 @@
+function summation(){
+    let a=prompt("Введите число");
+    let b=prompt("Введите число");
+    alert(Number(a) + Number(b));}
+
+function subtraction(){
+    let a=prompt("Введите число");
+    let b=prompt("Введите число");
+    alert((a)-(b));}
+    
+function increase(){
+    let a=prompt("Введите число");
+    let b=prompt("Введите число");
+    alert((a)*(b));}
+    
+    function divisio(){
+    let a=prompt("Введите число");
+    let b=prompt("Введите число");
+    alert((a)/(b));}
+            
