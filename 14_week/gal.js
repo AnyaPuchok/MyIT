96ccec70eb606f39f3079acc76a1ccad4ad0734f
Anna <89824460102@yandex.ru>
2021-09-11 23:58:24 +0500

    галерея

---
+++ 14_week/gal.js
@@ -0,0 +1,21 @@
+var mas = ["foto/1.jpg","foto/2.png","foto/3.jpg","foto/4.jpg","foto/5.jpg"] 
+var to = -1; 
+
+function right_arrow() 
+{ 
+ let obj = document.getElementById("img");
+  if (to < mas.length-1)  to++ 
+   else
+     to = 0;
+     obj.src = mas[to];	 
+}
+
+function left_arrow() 
+{     
+ let obj = document.getElementById("img");
+if (to > 0) to--;
+  else
+    to = mas.length-1;
+    obj.src = mas[to];	  			 
+}
+
