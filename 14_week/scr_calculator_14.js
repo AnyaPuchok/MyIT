e8d5dc5188245de3934e0db85368f807d085f496
Anna <89824460102@yandex.ru>
2021-09-13 19:03:51 +0500

    кальулятор с input

--- 14_week/scr_calculator_14.js
+++ 14_week/scr_calculator_14.js
@@ -1,28 +1,32 @@
 function summation() {
+    debugger;
     let a = parseInt(document.getElementById("num1").value);
     let b = parseInt(document.getElementById("num2").value);
-    let c = (Number(a) + Number(b));
-    var result = document.getElementById("result").innerText = c;
+    let c = ((a) - (b));
+    document.getElementById("result").innerText = c;
 }
 
 
 function subtraction() {
+    debugger;
     let a = parseInt(document.getElementById("num1").value);
     let b = parseInt(document.getElementById("num2").value);
     let c = ((a) - (b));
-    var result = document.getElementById("result").innerText = c;}
+    document.getElementById("result").innerText = c;}
 
     function increase() {
+        debugger;
         let a = parseInt(document.getElementById("num1").value);
         let b = parseInt(document.getElementById("num2").value);
         let c = ((a) * (b));
-        var result = document.getElementById("result").innerText = c;
+        document.getElementById("result").innerText = c;
     }
 
 
 function divisio() {
+    debugger;
     let a = parseInt(document.getElementById("num1").value);
     let b = parseInt(document.getElementById("num2").value);
     let c = ((a) / (b));
-    var result = document.getElementById("result").innerText = c;
+    document.getElementById("result").innerText = c;
 }
