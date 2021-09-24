fe639216121b93d7ef9ef0d5b2a76276d9274b25
Anna <89824460102@yandex.ru>
2021-09-24 20:34:52 +0500

    с замечаниями

--- 15_week/JS_list.js
+++ 15_week/JS_list.js
@@ -1,12 +1,20 @@
 function choice() {
-    if (document.getElementById("list").value === "White")
-        document.getElementById("result").style.backgroundColor = "White";
-    if (document.getElementById("list").value === "Pink")
-        document.getElementById("result").style.backgroundColor = "LightPink";
-    if (document.getElementById("list").value === "Blue")
-        document.getElementById("result").style.backgroundColor = "DeepSkyBlue";
-    if (document.getElementById("list").value === "Violet")
-        document.getElementById("result").style.backgroundColor = "Violet";
-    if (document.getElementById("list").value === "Green")
-        document.getElementById("result").style.backgroundColor = "PaleGreen";
+    let result = document.getElementById("result");
+    let list = document.getElementById("list");
+
+    if (list.value === "White") {
+        result.style.backgroundColor = "White";
+    }
+    if (list.value === "Pink") {
+        result.style.backgroundColor = "LightPink";
+    }
+    if (list.value === "Blue") {
+        result.style.backgroundColor = "DeepSkyBlue";
+    }
+    if (list.value === "Violet") {
+        result.style.backgroundColor = "Violet";
+    }
+    if (list.value === "Green") {
+        result.style.backgroundColor = "PaleGreen";
+    }
 }
--- 15_week/scr_calculator.js
+++ 15_week/scr_calculator.js
@@ -1,32 +1,34 @@
 function summation() {
     let a = parseInt(document.getElementById("num1").value);
     let b = parseInt(document.getElementById("num2").value);
-    let c = (Number(a) + Number(b));
-    var result = document.getElementById("result").innerText = c;
+    let c = (a + b);
+    document.getElementById("result").innerText = c;
 }
 
 
 function subtraction() {
     let a = parseInt(document.getElementById("num1").value);
     let b = parseInt(document.getElementById("num2").value);
-    let c = ((a) - (b));
-    let result = document.getElementById("result").innerText = c;
+    let c = (a - b);
+    document.getElementById("result").innerText = c;
 }
 
 function increase() {
     let a = parseInt(document.getElementById("num1").value);
     let b = parseInt(document.getElementById("num2").value);
-    let c = ((a) * (b));
-    var result = document.getElementById("result").innerText = c;
+    let c = (a * b);
+    document.getElementById("result").innerText = c;
 }
 
 
 function divisio() {
     let a = parseInt(document.getElementById("num1").value);
     let b = parseInt(document.getElementById("num2").value);
-    let c = ((a) / (b));
-    var result = document.getElementById("result").innerText = c;
-    if (result == `Infinity`) {
-        var result = document.getElementById("result").innerText = ("На ноль делить нельзя")
+    let c = (a / b);
+    document.getElementById("result").innerText = c;
+    if (c == `Infinity`) {
+        document.getElementById("result").innerText = ("На ноль делить нельзя")
+    } else {
+        document.getElementById("result").innerText = c;
     }
 }
